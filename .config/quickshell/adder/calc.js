96c6067d936fe3bb67374fbdb14ce31cab9e53bd
Buck <kbuck@tuta.io>
2026-08-27 13:32:33 +0300

    Add files via upload

---
+++ .config/quickshell/adder/calc.js
@@ -0,0 +1,12 @@
+// ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+// ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+// └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+// https://github.com/kbuckleys/
+
+function shellQuote(s) {
+  return "'" + String(s).replace(/'/g, "'\\''") + "'";
+}
+
+function escapeHtml(s) {
+  return String(s).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
